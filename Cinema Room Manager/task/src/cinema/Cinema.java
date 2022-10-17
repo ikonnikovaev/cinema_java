9492d8ce99a2e975b3080e8a13b27d3015e7e3f2
elena <ikonnikovaev@gmail.com>
2022-10-17 10:44:22 +0300

    Stage 5: statistics

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -34,11 +34,37 @@ public class Cinema {
         return CHEAP;
     }
 
-    public static void buyTicket(int r, int s) {
+    public static void buyTicket() {
+        int nRows = room.length;
+        int seatsPerRow = room[0].length;
+        boolean validSeat = false;
+        int r = -1;
+        int s = -1;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("Enter a row number:");
+            r = scanner.nextInt();
+            System.out.println("Enter a seat number in that row:");
+            s = scanner.nextInt();
+            if (r <= 0 || s <= 0 || r > nRows || s > seatsPerRow) {
+                System.out.println("Wrong input!");
+            } else if (room[r - 1][s - 1] == 'B') {
+                    System.out.println("That ticket has already been purchased!");
+            } else {
+                validSeat = true;
+            }
+        } while (!validSeat);
+
+        System.out.println("Ticket price: $" + getPrice(r, s));
         room[r - 1][s - 1] = 'B';
     }
 
-    public static void createRoom(int nRows, int seatsPerRow) {
+    public static void createRoom() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        int nRows = scanner.nextInt();
+        System.out.println("Enter the number of seats in each row:");
+        int seatsPerRow = scanner.nextInt();
         room = new char[nRows][seatsPerRow];
         for (int i = 0; i < nRows; ++i) {
             for (int j = 0; j < seatsPerRow; ++j) {
@@ -65,43 +91,60 @@ public class Cinema {
             }
             System.out.println();
         }
+    }
 
+    public static void printStatistics() {
+        int purchasedTickets = 0;
+        int currentIncome = 0;
+        int totalIncome = 0;
+        int nRows = room.length;
+        int seatsPerRow = room[0].length;
+        for (int r = 1; r <= nRows; ++r) {
+            for (int s = 1; s <= seatsPerRow; ++s) {
+                if (room[r - 1][s - 1] == 'B') {
+                    purchasedTickets += 1;
+                    currentIncome += getPrice(r, s);
+                }
+                totalIncome += getPrice(r, s);
+            }
+        }
+        double percentage = ((double) purchasedTickets) * 100 / (nRows * seatsPerRow);
+        System.out.println("Number of purchased tickets: %d".formatted(purchasedTickets));
+        System.out.println("Percentage: %.2f%%".formatted(percentage));
+        System.out.println("Current income: $%d".formatted(currentIncome));
+        System.out.println("Total income: $%d".formatted(totalIncome));
+    }
+
+    public static int chooseAction() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("1. Show the seats\n" +
+                "2. Buy a ticket\n" +
+                "3. Statistics\n" +
+                "0. Exit");
+        int action = scanner.nextInt();
+        switch (action) {
+            case 1:
+                printRoom();
+                break;
+            case 2:
+                buyTicket();
+                break;
+            case 3:
+                printStatistics();
+                break;
+            case 0:
+                break;
+        }
+        return action;
     }
 
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the number of rows:");
-        int nRows = scanner.nextInt();
-        System.out.println("Enter the number of seats in each row:");
-        int seatsPerRow = scanner.nextInt();
-        createRoom(nRows, seatsPerRow);
+        createRoom();
         printRoom();
-
-        System.out.println("Enter a row number:");
-        int r = scanner.nextInt();
-        System.out.println("Enter a seat number in that row:");
-        int s = scanner.nextInt();
-        System.out.println("Ticket price: $" + getPrice(r, s));
-        buyTicket(r, s);
-        printRoom();
-
-        // System.out.println("Total income:");
-        // System.out.println("$" + profit(nRows, seatsPerRow));
-
-
-        // Write your code here
-        /*
-        System.out.println("Cinema:");
-        System.out.println("  1 2 3 4 5 6 7 8");
-        System.out.println("1 S S S S S S S S");
-        System.out.println("2 S S S S S S S S");
-        System.out.println("3 S S S S S S S S");
-        System.out.println("4 S S S S S S S S");
-        System.out.println("5 S S S S S S S S");
-        System.out.println("6 S S S S S S S S");
-        System.out.println("7 S S S S S S S S");
-        */
+        int action = -1;
+        while (action != 0) {
+            action = chooseAction();
+        }
 
     }
 }
